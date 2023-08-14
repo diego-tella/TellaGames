7c2b3b7cc700bf10dec11d6fedd45ec86f704523
diego-tella <diegotellaroli05@gmail.com>
2023-08-14 11:24:59 -0300

    Fixed snake game. Background added

---
+++ games/Snake/README.md
@@ -0,0 +1,7 @@
+A simple clone of the snake game, developed as my first attempt at using `canvas` and Javascript.
+
+It was originally intended for the [0hgame jam](http://0hgame.eu), but couldn't make it on time. 
+
+You could try it live before, but it doesn't work in modern browsers anymore.
+
+Thankful mention to [Bill Mill's tutorial](http://billmill.org/static/canvastutorial/), my starting point.
--- games/Snake/index.php
+++ games/Snake/index.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<html>
+	<head>
+		<title>Snake</title>
+		<script type="text/javascript" src="jquery-1.8.2.js"></script>
+		<script type="text/javascript" src="lib.js"></script>
+		<script type="text/javascript" src="game.js"></script>
+		<link rel="stylesheet" type="text/css" href="../../css/style.css" media="screen" />
+    <link href="https://fonts.googleapis.com/css?family=Squada+One&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
-    <title>Snake Game</title>
-    <link rel="stylesheet" href="style.css">
-    <script defer src="script.js"></script>
-</head>
-<body>
-    <canvas id="snake" width="512" height="512"></canvas>
-</body>
+  
+	</head>
+	<body class="vh-100 gradient-custom" style="text-align:center">
+	<?php include "../../header.php"; ?><br><br><br>
+	<canvas id="canvas" width="400" height="400" style="background-color:black"></canvas>
+	</body>
 </html>
